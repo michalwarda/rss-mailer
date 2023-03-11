c6daf0306e751a958657298a6fe2d3d14a189ea0
Michał Warda <michal.warda@elpassion.pl>
2023-03-11 15:14:10 +0100

    Move client to other file

---
+++ RssClient.ts
@@ -0,0 +1,40 @@
+import { XMLParser } from "fast-xml-parser";
+import * as z from "zod";
+
+export class RssClient {
+  static FeedFailedToReturnError = class extends Error {};
+  static FeedNotFoundError = class extends RssClient.FeedFailedToReturnError {};
+  static InvalidFeedError = class extends Error {};
+
+  constructor(private readonly httpClient: () => Promise<Response>) {}
+
+  getFeed(feed: string) {
+    return this.httpClient()
+      .then((res) => {
+        if (res.status === 404) throw new RssClient.FeedNotFoundError();
+        if (res.status !== 200) throw new RssClient.FeedFailedToReturnError();
+        return res.text();
+      })
+      .then((text) => {
+        const parser = new XMLParser();
+        const json = parser.parse(text);
+        const schema = z.object({
+          rss: z.object({
+            channel: z.object({
+              item: z.array(
+                z.object({
+                  title: z.string(),
+                  link: z.string(),
+                  description: z.string(),
+                  pubDate: z.string(),
+                })
+              ),
+            }),
+          }),
+        });
+        const result = schema.safeParse(json);
+        if (!result.success) throw new RssClient.InvalidFeedError();
+        return result.data;
+      });
+  }
+}
--- test/RssClient.test.ts
+++ test/RssClient.test.ts
@@ -1,45 +1,7 @@
 import { describe, expect, test } from "bun:test";
-import * as z from "zod";
-import { XMLParser } from "fast-xml-parser";
 import * as fs from "fs";
 import { promisify } from "util";
-
-class RssClient {
-  static FeedFailedToReturnError = class extends Error {};
-  static FeedNotFoundError = class extends RssClient.FeedFailedToReturnError {};
-  static InvalidFeedError = class extends Error {};
-  constructor(private readonly httpClient: () => Promise<Response>) {}
-
-  getFeed(feed: string) {
-    return this.httpClient()
-      .then((res) => {
-        if (res.status === 404) throw new RssClient.FeedNotFoundError();
-        if (res.status !== 200) throw new RssClient.FeedFailedToReturnError();
-        return res.text();
-      })
-      .then((text) => {
-        const parser = new XMLParser();
-        const json = parser.parse(text);
-        const schema = z.object({
-          rss: z.object({
-            channel: z.object({
-              item: z.array(
-                z.object({
-                  title: z.string(),
-                  link: z.string(),
-                  description: z.string(),
-                  pubDate: z.string(),
-                })
-              ),
-            }),
-          }),
-        });
-        const result = schema.safeParse(json);
-        if (!result.success) throw new RssClient.InvalidFeedError();
-        return result.data;
-      });
-  }
-}
+import { RssClient } from "../RssClient.js";
 
 describe(RssClient.name, function () {
   describe(RssClient.prototype.getFeed.name, function () {
