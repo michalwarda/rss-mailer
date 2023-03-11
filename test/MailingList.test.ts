9181f5ce51c8499a7077edfa8f1e175735affb02
Michał Warda <michal.warda@elpassion.pl>
2023-03-11 16:04:28 +0100

    Pass correct MailingList

--- test/MailingList.test.ts
+++ test/MailingList.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, test } from "bun:test";
 
 class MailingList {
+  constructor(param: { append: (email: string) => Promise<void> }) {}
+
   static FailedToSubscribeError = class extends Error {};
   subscribe(email: string) {
     throw new MailingList.FailedToSubscribeError();
@@ -10,9 +12,9 @@ class MailingList {
 describe(MailingList.name, function () {
   describe(MailingList.prototype.subscribe.name, function () {
     test(`throws ${MailingList.FailedToSubscribeError.name} when something goes wrong`, async function () {
-      expect(() => new MailingList().subscribe("")).toThrow(
-        MailingList.FailedToSubscribeError as any as Error
-      );
+      expect(() =>
+        new MailingList({ append: async (email) => {} }).subscribe("")
+      ).toThrow(MailingList.FailedToSubscribeError as any as Error);
     });
   });
 });
