a2eb4dba9afba0b06a672be0c13d14605539d06c
Michał Warda <michal.warda@elpassion.pl>
2023-03-11 14:54:14 +0100

    [Refactor] Name error

--- test/RssClient.test.ts
+++ test/RssClient.test.ts
@@ -1,23 +1,24 @@
 import { describe, expect, test } from "bun:test";
 
 class RssClient {
+  static FeedNotFoundError = class extends Error {};
   constructor(private readonly httpClient: () => Promise<Response>) {}
 
   getFeed(feed: string) {
     return this.httpClient().then((res) => {
-      throw new Error("Feed not found");
+      throw new RssClient.FeedNotFoundError();
     });
   }
 }
 
 describe(RssClient.name, function () {
   describe(RssClient.prototype.getFeed.name, function () {
-    test("throws FeedNotFoundError when feed is not found", async function () {
+    test(`throws ${RssClient.FeedNotFoundError.name} when client returns 404`, async function () {
       const client = new RssClient(
         async () => new Response("", { status: 404 })
       );
       expect(() => client.getFeed("https://unknown.feed")).toThrow(
-        new Error("Feed not found")
+        new RssClient.FeedNotFoundError()
       );
     });
   });
