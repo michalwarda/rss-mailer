1fefa6eb571ecfad2e7a3b0af4318258cfa020bd
Michał Warda <michal.warda@elpassion.pl>
2023-03-11 14:59:51 +0100

    [Refactor] Handle feed error when non 200 status is returned and body is incorrect

--- test/RssClient.test.ts
+++ test/RssClient.test.ts
@@ -1,13 +1,21 @@
 import { describe, expect, test } from "bun:test";
 
 class RssClient {
-  static FeedNotFoundError = class extends Error {};
+  static FeedFailedToReturnError = class extends Error {};
+  static FeedNotFoundError = class extends RssClient.FeedFailedToReturnError {};
+  static InvalidFeedError = class extends Error {};
   constructor(private readonly httpClient: () => Promise<Response>) {}
 
   getFeed(feed: string) {
-    return this.httpClient().then((res) => {
-      throw new RssClient.FeedNotFoundError();
-    });
+    return this.httpClient()
+      .then((res) => {
+        if (res.status === 404) throw new RssClient.FeedNotFoundError();
+        if (res.status !== 200) throw new RssClient.FeedFailedToReturnError();
+        return res.text();
+      })
+      .then(() => {
+        throw new RssClient.InvalidFeedError();
+      });
   }
 }
 
@@ -18,7 +26,25 @@ describe(RssClient.name, function () {
         async () => new Response("", { status: 404 })
       );
       expect(() => client.getFeed("https://unknown.feed")).toThrow(
-        new RssClient.FeedNotFoundError()
+        RssClient.FeedNotFoundError
+      );
+    });
+
+    test(`throws ${RssClient.FeedFailedToReturnError.name} when client returns non 200 status`, async function () {
+      const client = new RssClient(
+        async () => new Response("", { status: 500 })
+      );
+      expect(() => client.getFeed("https://unknown.feed")).toThrow(
+        RssClient.FeedFailedToReturnError
+      );
+    });
+
+    test(`throws ${RssClient.InvalidFeedError.name} when client returns 200 but feed body is invalid`, () => {
+      const client = new RssClient(
+        async () => new Response("invalid feed", { status: 200 })
+      );
+      expect(() => client.getFeed("https://invalid.feed")).toThrow(
+        RssClient.InvalidFeedError
       );
     });
   });
