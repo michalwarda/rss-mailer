56ec2105a4bda8daa8d01040fb23ab2f9da3698b
Michał Warda <michal.warda@elpassion.pl>
2023-03-11 23:44:35 +0100

    Fix cron timing

--- lib/main.ts
+++ lib/main.ts
@@ -29,7 +29,7 @@ app
     })
   )
   .use(swagger())
-  .cron({ name: "Send mails", pattern: "* * * * * *" }, async () => {
+  .cron({ name: "Send mails", pattern: "* * * * *" }, async () => {
     await new RssClient(fetch)
       .getFeed("https://grifel.dev/rss.xml")
       .then((feed) => console.log(feed));
